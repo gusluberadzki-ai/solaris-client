d9d8b4584742114504de1082f300531340b87801
Gustaw Luberadzki <gusluberadzki@Gustaws-MacBook-Air.local>
2026-04-14 08:14:05 +0200

    Initial commit

---
+++ name_displayerV3.py
@@ -0,0 +1,7 @@
+name = input("Please state your name in lowercase: ")
+surname = input("Please state your surname in lowercase: ")
+
+name = name.title()
+surname = surname.title()
+
+print(name + " " + surname)
---
+++ uppercaseword.py
@@ -0,0 +1,3 @@
+word = input("Please state a word in lowercase: ")
+word = word.upper()
+print(word)
